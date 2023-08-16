c5fa385f8129bf5d5d23d4425a9911efdacbcb2c
Timofey Nagaev <nagaevmix33@gmail.com>
2023-08-16 21:27:16 +0700

    some changes

--- client/src/App/Nav/Nav.jsx
+++ client/src/App/Nav/Nav.jsx
@@ -2,31 +2,35 @@ import React from "react"
 
 import './Styles.css'
 
+import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavLink from "react-bootstrap/esm/NavLink";
 
 function SiteNav (props) {
     return (
-        <Navbar expand="lg" bg="primary" data-bs-theme="dark">
+        <Navbar expand="lg" bg="bg-primary" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">MangaBook</Navbar.Brand>
+            <Navbar.Brand href="#home" className="fs-4">MangaBook</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
+              <Nav className="w-100 fs-5">
                 <NavDropdown title="Каталог" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/1.1" >Манги</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/1.2">Манхвы</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/1.3">OEL</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/1.4">Комиксы</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/1.4">Случайный тайтл</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Ранобэ</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Книги</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/1.4">Случайный тайтл</NavDropdown.Item>
                 </NavDropdown>
+                <NavLink href="#" className="ms-auto" >Добавить тайтл</NavLink>
+                <div className="vr text-dark" />
+                <NavLink href="#">Войти</NavLink>
               </Nav>
             </Navbar.Collapse>
           </Container>
