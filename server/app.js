b7f7e698f14d91fa86f198c98f5bcf26f74dcfd8
Timofey Nagaev <nagaevmix33@gmail.com>
2023-08-16 14:46:57 +0700

    added server repos

---
+++ server/app.js
@@ -0,0 +1,45 @@
+const express = require('express'),
+port = '5000',
+app = express(),
+fs = require('fs'),
+path = require('path');
+const fileUpload = require('express-fileupload');
+//ee
+app.use(fileUpload({
+    defCharset: 'utf8',
+    defParamCharset: 'utf8'
+  }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/static', express.static('./library'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+    })
+app.post('/file', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+
+  let sampleFile = req.files.file;
+  console.log('added ' + req.body.bookname);
+
+  fs.mkdir(path.join(__dirname, 'library', req.body.bookname), () => {
+    // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(path.join(__dirname, "library", req.body.bookname, sampleFile.name), function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+  })
+  
+});
+
+
+app.listen(port, function () {
+    console.log(`server listen: http://${port}`);
+});
