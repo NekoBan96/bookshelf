4cf1887a7303bff60c9f9fcf0e973117ebaeb16e
NekoBan96 <telgrigory@gmail.com>
2023-08-17 00:32:42 +0700

    add: unpacking files

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ fs = require('fs'),
 path = require('path');
 const { log } = require('console');
 const fileUpload = require('express-fileupload');
+const zl = require("zip-lib");
+
+
 app.use(fileUpload({
     defCharset: 'utf8',
     defParamCharset: 'utf8'
@@ -23,18 +26,23 @@ app.post('/uploadContent', function(req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
-
-
   let sampleFile = req.files.file;
+  const pathFile = path.join(__dirname, "library", req.body.bookname, sampleFile.name)
+  const pathRep = path.join(__dirname, 'library', req.body.bookname)
   console.log('added ' + req.body.bookname);
-
-  fs.mkdir(path.join(__dirname, 'library', req.body.bookname), () => {
+  log(sampleFile)
+  fs.mkdir(pathRep, () => {
     // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(path.join(__dirname, "library", req.body.bookname, sampleFile.name), function(err) {
+  sampleFile.mv(pathFile, function(err) {
     if (err)
       return res.status(500).send(err);
 
     res.send('File uploaded!');
+    try{
+        zl.extract(pathFile, pathRep, 'utf8').then(()=> {
+          fs.rm(pathFile, ()=>{log('unzip done')})
+        }, (err)=>{log(err)})
+    }catch(err){log(err)}
   });
   })
   
