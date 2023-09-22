7491d218a4c1f2e5247fb4e82e1c071e8ffa8538
nagaev1 <nagaevmix33@gmail.com>
2023-09-22 18:42:00 +0700

    changes in ReadPage

--- client/src/App/main/ReadPage/ReadPage.jsx
+++ client/src/App/main/ReadPage/ReadPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import Form from "react-bootstrap/esm/Form"
 
 import ReadView from "../../../Components/ReadView/ReadView";
 
@@ -14,14 +15,13 @@ export default class ReadPage extends React.Component {
 
     render() {
         return(
-            <Container>
-                <Row className="justify-content-center">
-                    <Col xs="12" md="12" >
-                        <ReadView />
-                    </Col>
-                </Row>
+            <>
                 
-            </Container>
+                        <ReadView />
+              
+                
+                
+            </>
         )
     }
 }
--- client/src/Components/ReadView/ReadView.jsx
+++ client/src/Components/ReadView/ReadView.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+import "./ReadView.css"
+
 import Carousel from 'react-bootstrap/Carousel';
+import Form from 'react-bootstrap/esm/Form'
 
 import page from "./page.jpg"
 
@@ -12,17 +15,27 @@ export default class ReadView extends React.Component{
     render(){
         return (
 
-            <Carousel className="m-3 d-flex justify-content-center">
-                <Carousel.Item>
-                    <img src={page} className="mw-100" alt="page" />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img src={page} className="mw-100" alt="page" />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img src={page} className="mw-100" alt="page" />
-                </Carousel.Item>
-            </Carousel>
+            <>
+                <Form.Select className="position-fixed bottom-0 m-5" data-bs-theme="dark" style={{width: "5rem", zIndex: "9999"}}>
+                        <option value="">1/4</option>
+                        <option value="">2/4</option>
+                        <option value="">3/4</option>
+                        <option value="">4/4</option>
+                </Form.Select>
+                <Carousel interval={null} indicators={false} touch={true} keyboard={true} className="w-100" prevIcon={null} nextIcon={null}>
+                    <Carousel.Item className="text-center">
+                        <img src={page}  alt="page" className="mw-100" />
+                    </Carousel.Item>
+                    <Carousel.Item  className="text-center">
+                        <img src={page} alt="page" className="mw-100" />
+                    </Carousel.Item>
+                    <Carousel.Item className="text-center">
+                        <img src={page} alt="page" className="mw-100" />
+                    </Carousel.Item>
+                </Carousel>
+                
+              
+            </>
         )
     }
 }
