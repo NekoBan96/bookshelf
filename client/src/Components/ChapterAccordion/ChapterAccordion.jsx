8607abaac42d41793a7870251b06e1ff3ccaa035
nagaev1 <nagaevmix33@gmail.com>
2023-09-24 15:53:55 +0700

    complete accordion, almost complete readPage

--- client/src/Components/ChapterAccordion/ChapterAccordion.jsx
+++ client/src/Components/ChapterAccordion/ChapterAccordion.jsx
@@ -1,32 +1,66 @@
 import React from "react";
 
 import Accordion from 'react-bootstrap/Accordion';
+import { Link } from "react-router-dom";
 
 
 
 export default class ChapterAccordion extends React.Component{
     constructor(props){
         super(props)
+
+        this.state = {
+            volumes: [
+                [
+                    {
+                        name: "Глава 1 Начало",
+                        url: "read"
+                    },
+                    {
+                        name: "Глава 2 Поворот",
+                        url: "read"
+                    },
+                ],
+                [
+                    {
+                        name: "Глава 3 Пиздец",
+                        url: "read"
+                    },
+                    {
+                        name: "Глава 4 Конец",
+                        url: "read"
+                    },
+                ]
+            ]        
+        }
     }
 
+    createItems() {
+        let i = 0
+        return this.state.volumes.map(volume => {
+            i++
+            return <Accordion.Item key={volume} eventKey={i}>
+                <Accordion.Header>Том {i}</Accordion.Header>
+                <Accordion.Body>
+                    {volume.map(chapter => {
+                        return <p><Link to={chapter.url} className="text-light">{chapter.name}</Link></p>
+                    })}
+                </Accordion.Body>
+                
+            </Accordion.Item>
+        })
+    }
+
+
+
     render() {
+
+
+        
+
         return(
             <Accordion>
-                <Accordion.Item  eventKey="0">
-                    <Accordion.Header>Том 1</Accordion.Header>
-                    
-                    <Accordion.Body >
-                        <p><a href="#" className="text-light">глава 1</a></p>
-                        <p><a href="#" className="text-light">глава 2</a></p>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Том 2</Accordion.Header>
-                        <Accordion.Body>
-                        <p><a href="#" className="text-light">глава 3</a></p>
-                        <p><a href="#" className="text-light">глава 4</a></p>
-                        </Accordion.Body>
-                </Accordion.Item>
+                {this.createItems()}
             </Accordion>
         )
     }
--- client/src/Components/ReadView/ReadView.jsx
+++ client/src/Components/ReadView/ReadView.jsx
@@ -4,38 +4,94 @@ import "./ReadView.css"
 
 import Carousel from 'react-bootstrap/Carousel';
 import Form from 'react-bootstrap/esm/Form'
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 import page from "./page.jpg"
 
 export default class ReadView extends React.Component{
     constructor(props){
         super(props)
+
+        this.state = {
+            style: {
+                width: "50%"
+            },
+            sliderIndex: 1
+        }
+        this.handleRangeChange = this.handleRangeChange.bind(this)
+        this.handleSelect = this.handleSelect.bind(this)
+    }
+
+    handleRangeChange(e){
+        console.log(e.target.value);
+        this.setState({style: {width: e.target.value + "%"}})
+    }
+    handleSelect(selectedIndex){
+        
     }
 
     render(){
         return (
 
-            <>
-                <Form.Select className="position-fixed bottom-0 m-5" data-bs-theme="dark" style={{width: "5rem", zIndex: "9999"}}>
+            <div className="read-ui">
+                
+                <div className="position-fixed bottom-0 m-5">
+                    <Form.Select className="" data-bs-theme="dark" style={{width: "5rem", zIndex: "9999"}}>
                         <option value="">1/4</option>
                         <option value="">2/4</option>
                         <option value="">3/4</option>
                         <option value="">4/4</option>
-                </Form.Select>
-                <Carousel interval={null} indicators={false} touch={true} keyboard={true} className="w-100" prevIcon={null} nextIcon={null}>
-                    <Carousel.Item className="text-center">
-                        <img src={page}  alt="page" className="mw-100" />
-                    </Carousel.Item>
-                    <Carousel.Item  className="text-center">
-                        <img src={page} alt="page" className="mw-100" />
-                    </Carousel.Item>
-                    <Carousel.Item className="text-center">
-                        <img src={page} alt="page" className="mw-100" />
-                    </Carousel.Item>
-                </Carousel>
+                    </Form.Select>                    
+                </div>
+                
+
+
+                <Dropdown className="position-fixed bottom-0 end-0 m-5 d-none d-md-block" data-bs-theme="dark" style={{zIndex: "9999"}}>
+                    <Dropdown.Toggle variant="dark">
+                        Settings
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.ItemText>
+                            <Form.Group>
+                                <Form.Label>Размер контейнера</Form.Label>
+                                <Form.Range  min={30} data-bs-theme="dark" style={{width: "12rem", zIndex: "9999"}} onChange={this.handleRangeChange}/>
+                            </Form.Group>
+                            
+                        </Dropdown.ItemText>
+                    </Dropdown.Menu>
+                </Dropdown>
+                
+                
+                <div className="d-flex justify-content-center">
+                    
+                    <Carousel interval={null}
+                        indicators={false}
+                        touch={true}
+                        keyboard={true}
+                        className="read-view"
+                        style={this.state.style}
+                        prevIcon={null}
+                        nextIcon={null}
+                        onSelect={this.handleSelect}
+                        wrap={false}
+                        value={this.state.sliderIndex}
+                    >
+                        
+                        <Carousel.Item className="text-center">
+                            <img src={page}  alt="page" className="w-100" />
+                        </Carousel.Item>
+                        <Carousel.Item  className="text-center">
+                            <img src={page} alt="page" className="w-100" />
+                        </Carousel.Item>
+                        <Carousel.Item className="text-center">
+                            <img src={page} alt="page" className="w-100" />
+                        </Carousel.Item>
+                    </Carousel>                
+                </div>
                 
               
-            </>
+            </div>
         )
     }
 }
