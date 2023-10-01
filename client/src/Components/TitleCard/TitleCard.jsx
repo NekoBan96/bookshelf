8d9374208798d3e791a68a354d5b3502fe92c7bc
Timofey Nagaev <nagaevmix33@gmail.com>
2023-10-01 18:10:05 +0700

    added img holder to card

--- client/src/Components/TitleCard/TitleCard.jsx
+++ client/src/Components/TitleCard/TitleCard.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {Card, Row, Col} from "react-bootstrap/esm"
 import { Link } from "react-router-dom";
+import { run as runHolder } from 'holderjs/holder';
 
 export default function TitleCard(props) {
+    useEffect(() => {        
+        runHolder('imgHolder');
+    });
 
     let url = props.url || "/manga"
-    let img = props.img || "https://animego.org/media/cache/thumbs_250x350/upload/anime/images/64e1d7f767e31205809598.jpg"
+    let img = props.img || "holder.js/200x280?bg=1d334a&fg=138abd"
     let title = props.title || "Усопшие"
     let altTitle = props.altTitle || "Shiki"
     let description = props.discription || "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam facere nobis dicta numquam sunt! Vero, distinctio iusto aspernatur expedita velit nesciunt earum maxime sint, rem mollitia consectetur non culpa alias modi sunt eveniet, consequatur tenetur saepe repellat! Ea possimus est molestias fugit blanditiis obcaecati amet commodi fugiat asperiores eos quae in consequatur nam ex ullam ipsum dolorum assumenda nobis culpa placeat, similique quibusdam quos repellat debitis. Itaque omnis facere esse reiciendis consectetur velit dolorum, sed eos repellat. Fugit obcaecati culpa enim tenetur ut doloremque dignissimos, molestiae eaque velit sed sit debitis, nihil distinctio magnam odio ducimus dolorum. Veritatis, dignissimos dolore."
 
-    let CardImg = <Card.Link as={Link} to={url}>
+    let CardImg = <Card.Link as={Link} to={url} className="d-flex justify-content-center">
                         <Card.Img variant="top" src={img} style={{maxWidth: "20rem"}} />
                     </Card.Link>
     let CardTitle = <Card.Link as={Link} to={url}>
@@ -56,10 +60,10 @@ export default function TitleCard(props) {
             return (
                 <Card bg="bg-primary" text="text-primary">
                     <Row className="align-items-center">
-                            <Col className="align-items-center">
+                            <Col className="align-items-center" xs="6" sm="6" md="auto">
                                 {CardImg}
                             </Col>
-                            <Col xs="8" xl="10">
+                            <Col xs="6" sm="6" md="8">
                                 <Card.Body>
                                     {CardTitle}
                                     <Card.Title>{altTitle}</Card.Title>
