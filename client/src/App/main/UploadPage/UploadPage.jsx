4b83d04691c1760aaa202b8f4335ba568c54e08c
nagaev1 <nagaevmix33@gmail.com>
2023-10-05 17:26:55 +0700

    changes in upload form

--- client/src/App/main/UploadPage/UploadPage.jsx
+++ client/src/App/main/UploadPage/UploadPage.jsx
@@ -6,6 +6,7 @@ import Form from 'react-bootstrap/Form';
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import axios from 'axios';
 
@@ -45,20 +46,56 @@ export default class UploadPage extends React.Component {
             <Container className="my-5">
             <Form data-bs-theme="dark" method="post" action="http://localhost:5000/uploadContent" onSubmit={this.handleSubmit}>
                 <Row>
-                    <Col>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className="text-light" >Имя тайтла</Form.Label>
-                            <Form.Control type="text" placeholder="Токийский гуль" name="titleName" required="required"/>
+                    <Form.Group as={Col} lg="4" className="mb-3">
+                        <Form.Label className="text-light" >Название тайтла</Form.Label>
+                        <Form.Control type="text" placeholder="Токийский гуль" name="titleName" required="required"/>
+                    </Form.Group>
+                        <Form.Group as={Col} lg="4" className="mb-3">
+                            <Form.Label className="text-light" >Другое название тайтла (необ.)</Form.Label>
+                            <Form.Control type="text" placeholder="Tokyo ghoul" name="titleAltName" />
                         </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group>
+                        <Form.Group as={Col} lg="4"> 
                         <Form.Label className="text-light">Папка с содержимым</Form.Label>
                         <Form.Control type="file" accept=".rar, .zip" name="file" required="required"/>
                         </Form.Group>
-                    </Col>
                 </Row>
                 <Row>
+                    <Form.Group as={Col}>
+                        <Form.Label className="text-light">Описание</Form.Label>
+                        <Form.Control as="textarea" name="description" rows={5} />
+                    </Form.Group>
+                </Row>
+                <Row className="mt-3">
+                    <Form.Group as={Col}>
+                        <Form.Label className="text-light">Жанры</Form.Label>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="dark">
+                                Выберете жанр
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.ItemText>
+                                    <Form.Check
+                                        type="checkbox"
+                                        label="Драма"
+                                    />
+                                </Dropdown.ItemText>
+                                <Dropdown.ItemText>
+                                    <Form.Check
+                                        type="checkbox"
+                                        label="Комедия"
+                                    />
+                                </Dropdown.ItemText>
+                                <Dropdown.ItemText>
+                                    <Form.Check
+                                        type="checkbox"
+                                        label="Сёдзе"
+                                    />
+                                </Dropdown.ItemText>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Form.Group>
+                </Row>
+                <Row className="my-4">
                     <Col>
                         <Button variant="dark" type="submit">
                             Добавить
