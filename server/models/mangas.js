580d24dcd2e5b95d33eca28db5abcf1115aa9776
NekoBan96 <telgrigory@gmail.com>
2023-10-05 17:40:17 +0700

    fixed: num -> str

--- server/models/mangas.js
+++ server/models/mangas.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const Manga = new Schema ({
     titleName: { type: String, required: true },
     titleAltName: { type: String, required: false },
-    description: { type: Number, required: false },
+    description: { type: String, required: false },
    // genre: { type: String, required: true },
 });
 module.exports = mongoose.model('manga', Manga)
