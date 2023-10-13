767dca17896051b9eba64c1ef3886fd0a64efa98
NekoBan96 <telgrigory@gmail.com>
2023-10-13 23:07:42 +0700

    idi nahui

--- server/app.js
+++ server/app.js
@@ -76,9 +76,13 @@ app.get('/db/recentAdded/', function (req, res) {
   db.recent(countTitles, start).then (result => {res.send(result)}, err => {throw new Error(err)})
 })
 
-app.get('db/search/', function (req, res) {
+app.get('/db/search/', function (req, res) {
   search = req.query.s
   db.searchByName(search).then (result => {res.send(result)}, err => {throw new Error(err)})
+})
+
+app.get('/db/searchGenre', function (req, res) {
+  res.send('иди нахуй')
 })
 
 app.listen(port, function () {
